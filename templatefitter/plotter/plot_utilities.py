a74ed92d22edcf37c8877ea717349e8a22e19b5a
Moritz Bauer <moritz.bauer@desy.de>
2023-01-16 20:18:51 +0100

    Added hierarchical labels to BinNuisance plots

--- templatefitter/plotter/plot_utilities.py
+++ templatefitter/plotter/plot_utilities.py
@@ -9,11 +9,13 @@ import matplotlib.pyplot as plt
 import matplotlib.axes._axes as axes
 import matplotlib.colors as mpl_colors
 
-from typing import Tuple
+from typing import Tuple, List, Optional
 from matplotlib import figure
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 from templatefitter.utility import PathType
 from templatefitter.plotter.plot_style import KITColors
+from templatefitter.binned_distributions.binning import Binning
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
@@ -103,3 +105,75 @@ def color_fader(
     c1 = np.array(mpl_colors.to_rgb(color_1))
     c2 = np.array(mpl_colors.to_rgb(color_2))
     return mpl_colors.to_hex((1 - mix) * c1 + mix * c2)
+
+
+def _convert_flat_binning_to_axis_hierarchy_and_labels(flat_binning: List[Tuple[float, ...]]):
+    """
+    Helper function to convert tuple-wise "flat" binning to format needed for hierarchical axes
+    """
+
+    right_bin_edge = [len(flat_binning)]
+    flat_binning = np.array(flat_binning)
+
+    labels_and_ranges = []
+    factor = 1
+    for binlevel in range(flat_binning.shape[1] - 1, -1, -1):
+        a = flat_binning[:, binlevel]
+        ranges, labels = zip(*enumerate(a[np.insert(np.diff(a).astype(np.bool), 0, True)]))
+
+        if labels_and_ranges:
+            prev = len(labels_and_ranges[-1][1])
+            factor *= int(prev / len(labels))
+
+        labels_and_ranges.append(([r * factor for r in ranges] + right_bin_edge, labels))
+
+    return labels_and_ranges
+
+
+def add_hierarchical_axes_to_plot(ax, x_binning: Binning, y_binning: Optional[Binning] = None, colors = None):
+    """
+    This is a somewhat hacky way to add multiple axis levels with color-coding to plots.
+    Inspired by this solution:
+     https://stackoverflow.com/questions/46507472/matplotlib-correlation-matrix-heatmap-with-grouped-colors-as-labels
+    """
+
+    if colors is None:
+        colors = KITColors.default_colors
+
+    # create axes next to plot
+    divider = make_axes_locatable(ax)
+
+    barkw = dict(linewidth=0.72, ec="k", clip_on=False, align='edge')
+    textkw = dict(ha="center", va="center", fontsize="small")
+
+    for xranges, xlabels in _convert_flat_binning_to_axis_hierarchy_and_labels(x_binning.get_flat_list_of_bins()):
+
+        binmids = xranges[:-1] + np.diff(xranges)/2
+        xax = divider.append_axes("bottom", "10%", pad=0.06, sharex=ax)
+        xax.invert_yaxis()
+        xax.axis("off")
+        xax.bar(xranges[:-1], np.ones(len(xlabels)),
+                width=np.diff(xranges), color=colors[:len(np.unique(xlabels))], **barkw)
+
+        for binmid, xlabel in zip(binmids, xlabels):
+            xax.text(binmid, 0.5 , xlabel, **textkw)
+
+    if y_binning is not None:
+        for yranges, ylabels in _convert_flat_binning_to_axis_hierarchy_and_labels(y_binning.get_flat_list_of_bins()):
+
+            binmids = yranges[:-1] + np.diff(yranges)/2
+            yax = divider.append_axes("left", "10%", pad=0.06, sharey=ax)
+            yax.invert_yaxis()
+            yax.axis("off")
+            yax.barh(yranges[:-1] ,np.ones(len(ylabels)),
+                     height=np.diff(yranges), **barkw)
+
+            for binmid, ylabel in zip(binmids, ylabels):
+                yax.text(0.5, binmid, ylabel,rotation=-90, **textkw)
+
+    ax.margins(0)
+    ax.tick_params(axis="both", bottom=0, left=0, labelbottom=0, labelleft=0)
+
+    return ax
+
+
