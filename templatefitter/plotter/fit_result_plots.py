Move bin scaling into relevant methods and apply to errors too. Fixed some type bugs to make mypy happy (but not all unfortunately)

templatefitter/plotter/fit_result_plots.py
@@ -91,12 +91,10 @@ class FitResultPlot(FitPlotBase):
     ) -> None:
         self._check_required_histograms()
 
-        bin_scaling = self.binning.get_bin_scaling()  # type: np.ndarray
-
         data_bin_count = self._histograms[self.data_key].get_bin_count_of_component(index=0)
         data_bin_errors_sq = self._histograms[self.data_key].get_histogram_squared_bin_errors_of_component(index=0)
 
-        mc_bin_counts = self._histograms[self.mc_key].get_bin_counts(factor=bin_scaling)
+        mc_bin_counts = self._histograms[self.mc_key].get_bin_counts()
         # clean_mc_bin_counts = [np.where(bc < 0., 0., bc) for bc in mc_bin_counts]
 
         mc_sum_bin_count = np.sum(np.array(mc_bin_counts), axis=0)
@@ -146,7 +144,7 @@ class FitResultPlot(FitPlotBase):
 
         ax1.errorbar(
             x=self.bin_mids,
-            y=data_bin_count * bin_scaling,
+            y=data_bin_count,
             yerr=np.sqrt(data_bin_errors_sq),
             xerr=self.bin_widths / 2 if markers_with_width else None,
             ls="",

templatefitter/plotter/fit_template_plots.py
@@ -92,9 +92,7 @@ class FitTemplatePlot(FitPlotBase):
     ) -> None:
         self._check_required_histograms()
 
-        bin_scaling = self.binning.get_bin_scaling()  # type: np.ndarray
-
-        template_bin_counts = self._histograms[self.hist_key].get_bin_counts(factor=bin_scaling)
+        template_bin_counts = self._histograms[self.hist_key].get_bin_counts()
         assert isinstance(template_bin_counts, list) and len(template_bin_counts) == 1, template_bin_counts
 
         mc_sum_bin_error_sq = self._histograms[self.hist_key].get_statistical_uncertainty_per_bin()
@@ -357,10 +355,10 @@ class FitTemplatesPlotter(FitPlotterBase):
 
     @staticmethod
     def _set_2d_axis_tick_labels(ax: AxesType, binning: Binning) -> None:
-        x_tick_positions = np.arange(binning.num_bins[0] + 1) - 0.5  # type: np.array
-        y_tick_positions = np.arange(binning.num_bins[1] + 1) - 0.5  # type: np.array
-        x_tick_labels = np.array(binning.bin_edges[0])  # type: np.array
-        y_tick_labels = np.array(binning.bin_edges[1])  # type: np.array
+        x_tick_positions = np.arange(binning.num_bins[0] + 1) - 0.5  # type: np.ndarray
+        y_tick_positions = np.arange(binning.num_bins[1] + 1) - 0.5  # type: np.ndarray
+        x_tick_labels = np.array(binning.bin_edges[0])  # type: np.ndarray
+        y_tick_labels = np.array(binning.bin_edges[1])  # type: np.ndarray
 
         ax.set_xticks(ticks=x_tick_positions)
         ax.set_yticks(ticks=y_tick_positions)

templatefitter/plotter/histogram.py
@@ -15,6 +15,7 @@ import matplotlib.pyplot as plt
 
 from collections import OrderedDict
 from typing import Optional, Union, List, Tuple, Dict, ItemsView, KeysView, ValuesView
+from numpy import number
 
 from templatefitter.utility import PathType
 from templatefitter.binned_distributions import distributions_utility
@@ -146,14 +147,10 @@ class Histogram:
         )
         self._binning = new_binning
 
-    def get_bin_counts(
-        self,
-        factor: Union[None, float, np.ndarray] = None,
-    ) -> List[np.ndarray]:
-        if factor is None:
-            return [component.get_histogram_bin_count(binning=self.binning) for component in self._components]
-        else:
-            return [component.get_histogram_bin_count(binning=self.binning) * factor for component in self._components]
+    def get_bin_counts(self) -> List[np.ndarray]:
+
+        bin_scaling = self.binning.get_bin_scaling()
+        return [component.get_histogram_bin_count(binning=self.binning) * bin_scaling for component in self._components]
 
     def get_bin_count_of_component(
         self,
@@ -166,20 +163,27 @@ class Histogram:
         index: int,
         normalization_factor: Optional[float] = None,
     ) -> np.ndarray:
-        return self._components[index].get_histogram_squared_bin_errors(
-            binning=self.binning,
-            normalization_factor=normalization_factor,
+        return (
+            self._components[index].get_histogram_squared_bin_errors(
+                binning=self.binning,
+                normalization_factor=normalization_factor,
+            )
+            * self.binning.get_bin_scaling()
         )
 
     def get_statistical_uncertainty_per_bin(self, normalization_factor: Optional[float] = None) -> np.ndarray:
-        return np.sum(
-            [
-                component.get_histogram_squared_bin_errors(
-                    binning=self.binning, normalization_factor=normalization_factor
-                )
-                for component in self._components
-            ],
-            axis=0,
+
+        return np.array(
+            np.sum(
+                [  # Extra np.array for mypy
+                    component.get_histogram_squared_bin_errors(
+                        binning=self.binning, normalization_factor=normalization_factor
+                    )
+                    * self.binning.get_bin_scaling()
+                    for component in self._components
+                ],
+                axis=0,
+            )
         )
 
     def get_systematic_uncertainty_per_bin(self) -> Optional[np.ndarray]:
@@ -284,8 +288,8 @@ class Histogram:
         if self._raw_data_scope is not None:
             return self._raw_data_scope
 
-        min_values = []  # type: List[float]
-        max_values = []  # type: List[float]
+        min_values = []  # type: List[number]
+        max_values = []  # type: List[number]
 
         if len(self._components) == 0:
             raise RuntimeError("No components available to derive raw data range from...")
@@ -303,7 +307,7 @@ class Histogram:
             min_values.append(np.amin(component.min_val))
             max_values.append(np.amax(component.max_val))
 
-        scope_tuple = (np.amin(min_values), np.amax(max_values))  # type: Tuple[float, float]
+        scope_tuple = (float(np.amin(min_values)), float(np.amax(max_values)))  # type: Tuple[float, float]
         self._raw_data_scope = scope_tuple
         return scope_tuple
 